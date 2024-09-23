e54a7a3cb7c2e638eccf27f0d6344ec3e8f7e9a9
ddarrkkaa <kundakadus@gmail.com>
2024-09-23 10:26:06 +0300

    1

---
+++ asets/index.js
@@ -0,0 +1,67 @@
+const swiper = new Swiper(".review__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper2 = new Swiper(".role__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper3 = new Swiper(".invest__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper4 = new Swiper(".benefits__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper5 = new Swiper(".benefits__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper6 = new Swiper(".result__swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
