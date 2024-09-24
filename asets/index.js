deb5bd2c402d5ec0046707c59c4d371eafb9135f
ddarrkkaa <kundakadus@gmail.com>
2024-09-24 11:34:35 +0300

    srac

--- asets/index.js
+++ asets/index.js
@@ -1,17 +1,39 @@
-const review__swiper = new Swiper(".review__swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 2,
-  spaceBetween: 24,
+// const reviewSwiper = new Swiper(".review__swiper", {
+//   direction: "horizontal",
+//   loop: false,
+//   slidesPerView: 2,
+//   spaceBetween: 24,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
-const role__swiper = new Swiper(".role__swiper", {
-  direction: "horizontal",
+// const roleSwiper = new Swiper(".role__swiper", {
+//   direction: "horizontal",
+//   loop: false,
+//   slidesPerView: 2,
+//   spaceBetween: 24,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
+// const investSwiper = new Swiper(".invest__swiper", {
+//   direction: "horizontal",
+//   loop: false,
+//   slidesPerView: 3,
+//   spaceBetween: 24,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
+const benefitsSwiper = new Swiper(".benefits__swiper", {
+  direction: "vertical",
   loop: false,
   slidesPerView: 2,
   spaceBetween: 24,
@@ -21,29 +43,7 @@ const role__swiper = new Swiper(".role__swiper", {
   },
 });
 
-const invest__swiper = new Swiper(".invest__swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 2,
-  spaceBetween: 24,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-const benefits__swiper = new Swiper(".benefits__swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 3,
-  spaceBetween: 24,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-const result__swiper = new Swiper(".result__swiper", {
+const resultSwiper = new Swiper(".result__swiper", {
   direction: "horizontal",
   loop: false,
   slidesPerView: 2,
