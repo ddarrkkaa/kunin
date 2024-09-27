1970f4a95cbaa3ae0ef933a868eb4299f9a6894a
ddarrkkaa <kundakadus@gmail.com>
2024-09-27 17:43:56 +0300

    adaptive, mobile fixed

--- asets/index.js
+++ asets/index.js
@@ -1,73 +1,87 @@
-// const benefitsSwiper = new Swiper(".benefits__swiper", {
-//   direction: "vertical",
-//   loop: false,
-//   slidesPerView: 2,
-//   spaceBetween: 24,
+new Swiper(".reviews__slider", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".review-button-next",
+    prevEl: ".review-button-prev",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 2,
+    },
+  },
+});
 
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-// });
-
-const reviewSwiper = new Swiper(".review__swiper", {
-  direction: "horizontal",
+new Swiper(".role__slider", {
   loop: false,
-  slidesPerView: 2,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".role-button-next",
+    prevEl: ".role-button-prev",
+  },
+  pagination: {
+    el: ".role-slide-pagination",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 1.5,
+    },
+    1400: {
+      slidesPerView: 2,
+    },
+    1600: {
+      slidesPerView: 2.5,
+    },
+  },
+});
+
+new Swiper(".benefits__slider", {
+  loop: false,
+  spaceBetween: 48,
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 1.5,
+    },
+    1400: {
+      slidesPerView: 2,
+    },
+    1600: {
+      slidesPerView: 2.5,
+    },
+  },
+});
+
+new Swiper(".result__swiper", {
+  direction: "horizontal",
+  loop: true,
   spaceBetween: 24,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: ".result-slide-pagination",
   },
 
   breakpoints: {
     375: {
       slidesPerView: 1,
     },
-  },
-});
-
-const roleSwiper = new Swiper(".role__swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 2.5,
-  spaceBetween: 24,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // breakpoints: {
-  //   375: {
-  //     slidesPerView: 1,
-  //   },
-  // },
-});
-
-const resultSwiper = new Swiper(".result__swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 3,
-  spaceBetween: 24,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // breakpoints: {
-  //   650: {
-  //     slidesPerView: 2,
-  //   },
-  // },
-
-  breakpoints: {
-    375: {
-      slidesPerView: 1,
+    650: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 2.5,
     },
   },
 });
